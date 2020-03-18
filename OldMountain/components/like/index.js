a50b1f94cc1e79f1c406c7dfe187f8a317fe2238
Hugo <lipeng@qiniu.com>
2020-03-18 23:29:54 +0800

    组件数据，事件与属性

--- OldMountain/components/like/index.js
+++ OldMountain/components/like/index.js
@@ -4,22 +4,35 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    like: {
+      type: Boolean,
+      value: false,
+    },
+    count: {
+      type: Number
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    yes_url: 'images/like.png',
+    no_url: 'images/like@dis.png'
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-    onLike:function(event){
-      console.log(event)
+    onLike: function(event) {
+      let like = this.properties.like
+      let count = this.properties.count
+      count = like ? (count - 1) : (count + 1)
+      this.setData({
+        count: count,
+        like: !like
+      }) 
     }
   }
 })
