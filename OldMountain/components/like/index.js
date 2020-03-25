619ea25a9b95d8a1752851aadb1c509cbf3fc084
Hugo <lipeng@qiniu.com>
2020-03-25 23:05:51 +0800

    OldMountain

--- OldMountain/components/like/index.js
+++ OldMountain/components/like/index.js
@@ -1,38 +1,34 @@
-// components/like/index.js
+// components/like/like-cmp.js
 Component({
   /**
    * 组件的属性列表
    */
   properties: {
-    like: {
-      type: Boolean,
-      value: false,
-    },
-    count: {
-      type: Number
-    }
+    like: Boolean,
+    count: Number,
+    readOnly:Boolean
   },
 
-  /**
-   * 组件的初始数据
-   */
   data: {
     yes_url: 'images/like.png',
     no_url: 'images/like@dis.png'
   },
 
-  /**
-   * 组件的方法列表
-   */
   methods: {
-    onLike: function(event) {
-      let like = this.properties.like
+    onLike: function (event) {
+      if(this.properties.readOnly){
+        return
+      }
       let count = this.properties.count
-      count = like ? (count - 1) : (count + 1)
+      count = this.properties.like ? count - 1 : count + 1
       this.setData({
         count: count,
-        like: !like
-      }) 
+        like: !this.properties.like
+      })
+      let behavior = this.properties.like ? 'like' : 'cancel'
+      this.triggerEvent('like', {
+        behavior: behavior
+      }, {})
     }
   }
 })
